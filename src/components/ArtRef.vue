abc1c9a3d184ffe3c5b89e48ed441af6558504a0
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-21 21:56:01 +0000

    lol

--- src/components/ArtRef.vue
+++ src/components/ArtRef.vue
@@ -1,40 +1,60 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 let urlbase = 'https://www.reddit.com/r/'
 let urlend = '/.json'
 
 const images = ref([])
 
+function isImgUrl(url) {
+  return /\.(jpe?g|png|webp|avif|gif)$/.test(url)
+}
+
 async function loadImagesFromSub(sub){
     let url = urlbase + sub + urlend;
-    let subImgs = [];
     fetch(url).then(res => res.json()).then(d =>{
         if(!d.data){return}
         d.data.children.forEach(c=>{
-            subImgs.push(c.data.url);
+            let imgUrl = c.data.url
+            if(isImgUrl(imgUrl)){
+                images.value.push(c.data.url);
+            }
         })
     })
-    // this.subImgs = subImgs;
-    // console.log(this.subImgs)
-    return subImgs;
 }
 async function getImages(){
-    this.loadImagesFromSub('mombod').then(s => {
-        images.value = s;
-        console.log(s);
-        return s
-    })
+    loadImagesFromSub('mombod')
 }
+
+onMounted(() => {
+    getImages()
+})
 </script>
 
 <template>
     <h1>lol</h1>
     <button @click="getImages()"></button>
-    <div v-for="img in images" :key="img">
-        <img :src="img"/>
+    <div class="images">
+        <div v-for="img in images" :key="img">
+            <img :src="img" class="image"/>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.image {
+    max-width: 600px;
+    margin: auto;
+    padding: 0.5rem 2rem;
+}
 
+.images {
+    /* width: 100vw;
+    padding: 0 2rem;
+    margin: auto;
+    display: flex;
+    flex-direction: column; */
+    /* justify-content: center;
+    align-items: center; */
+    /* background-color: red; */
+}
 </style>
